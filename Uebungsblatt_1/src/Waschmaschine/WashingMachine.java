f20da5eb93cafcb6cbe3831d61a2623c37e53e30
Jonathan Mendoza <john.2114@hotmail.com>
2025-03-29 13:33:08 +0100

    Demo Washmachine Project Updated

--- Uebungsblatt_1/src/Waschmaschine/WashingMachine.java
+++ Uebungsblatt_1/src/Waschmaschine/WashingMachine.java
@@ -1,85 +1,100 @@
 
 package Waschmaschine;
 
-import java.io.PrintStream;
-
 public class WashingMachine {
+    //attributen
     private State currentState;
     private Program selectedProgram;
     private Temperature selectedTemperaturWert;
 
+    //Konstruktor
     public WashingMachine() {
         this.currentState = State.OFF;
         this.selectedProgram = null;
         this.selectedTemperaturWert = null;
     }
 
+    //getter
     public State getCurrentState() {
         return this.currentState;
     }
 
     public void powerOn() {
-        System.out.println(this.getCurrentState());
-        if (this.currentState == State.OFF) {
-            this.currentState = State.READY;
+        System.out.println(getCurrentState());
+        if (currentState == State.OFF) {
+            currentState = State.READY;
         }
 
-        System.out.println("Maschine Status: " + String.valueOf(this.currentState));
+        System.out.println("Maschine Status: " + currentState);
     }
 
-    public void selectProgram(Program program) {
-        if (this.currentState == State.READY) {
-            this.selectedProgram = program;
-            switch (program) {
-                case DELICATE -> System.out.println("Program: " + String.valueOf(Program.DELICATE));
-                case NORMAL -> System.out.println("Program: " + String.valueOf(Program.NORMAL));
-                case QUICK_WASH -> System.out.println("Program: " + String.valueOf(Program.QUICK_WASH));
-            }
-        } else {
-            System.out.println(this.currentState = State.OFF);
-        }
 
+    public void selectProgram(Program program) {
+
+        if (currentState == State.READY) {
+            selectedProgram = program;
+            switch (program) {
+                case Program.DELICATE:
+                    System.out.println("Program: " + Program.DELICATE);
+                    break;
+                case Program.NORMAL:
+                    System.out.println("Program: " + Program.NORMAL);
+                    break;
+                case Program.QUICK_WASH:
+                    System.out.println("Program: " + Program.QUICK_WASH);
+                    break;
+            }
+
+        } else {
+            System.out.println(currentState = State.OFF);
+        }
     }
 
     public void setTemperature(Temperature temperature) {
-        if (this.currentState == State.READY) {
-            this.selectedTemperaturWert = temperature;
+
+        if (currentState == State.READY) {
+            selectedTemperaturWert = temperature;
             switch (temperature) {
-                case COLD:
-                    PrintStream var3 = System.out;
-                    String var5 = String.valueOf(Temperature.COLD);
-                    var3.println("Temperature:" + var5 + ": " + Temperature.COLD.getTemperaturWert() + "°C");
+
+                case Temperature.COLD:
+                    System.out.println("Temperature:" + Temperature.COLD +
+                            ": " + Temperature.COLD.getTemperaturWert() + "°C");
                     break;
-                case HOT:
-                    PrintStream var2 = System.out;
-                    String var4 = String.valueOf(Temperature.HOT);
-                    var2.println("Temperature: " + var4 + ": " + Temperature.HOT.getTemperaturWert() + "°C");
+                case Temperature.HOT:
+                    System.out.println("Temperature: " + Temperature.HOT +
+                            ": " + Temperature.HOT.getTemperaturWert() + "°C");
                     break;
-                case WARM:
-                    PrintStream var10000 = System.out;
-                    String var10001 = String.valueOf(Temperature.WARM);
-                    var10000.println("Temperature: " + var10001 + ": " + Temperature.WARM.getTemperaturWert() + "°C");
+
+                case Temperature.WARM:
+                    System.out.println("Temperature: " + Temperature.WARM +
+                            ": " + Temperature.WARM.getTemperaturWert() + "°C");
+                    break;
+
             }
         }
-
     }
 
     public void Start() {
-        if (this.currentState == State.READY && this.selectedProgram != null && this.selectedTemperaturWert != null) {
-            System.out.println("Wasching");
+          /*Alternativ
+        if(currentState == State.READY && selectedProgram ==  Program.NORMAL
+          && selectedTemperaturWert ==Temperature.WARM){
+         ---- etc
+             */
+        if (currentState == State.READY && selectedProgram != null && selectedTemperaturWert != null) {
+            System.out.println("Status: " + State.Running);
 
-            for(int count = 7; count >= 1; --count) {
-                System.out.println(count);
+            for(int timer = 7; timer >= 1; --timer) {
+                System.out.println("Timer: " +  timer);
 
                 try {
                     Thread.sleep(1000L);
-                } catch (InterruptedException var3) {
+                } catch (InterruptedException e) {
                     System.out.println("Washing interrupted");
                 }
             }
 
             System.out.println("Washing completed!");
-            System.out.println(this.currentState = State.OFF);
+            System.out.println(currentState = State.OFF);
         } else {
             System.out.println("Error");
         }
